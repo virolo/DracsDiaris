25d57408d45de18ca0b9a6d8b67e5523c22ded07
VictorRomero <victor@romero.tech>
2025-04-06 12:13:47 +0200

    Start to tinker with board positioning

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -2,6 +2,7 @@ using System.Collections.Generic;
 using DragonBook;
 using DragonBook.Factory;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class GameManager : MonoBehaviour
 {
@@ -10,7 +11,16 @@ public class GameManager : MonoBehaviour
 
     [SerializeField]
     private DragonBookFactoryView _dragonBookFactoryView;
-   
+
+    [SerializeField]
+    private Camera _camera;
+
+    [SerializeField]
+    private Transform _movableDrake;
+
+    [SerializeField]
+    private bool _enabled;
+    
     private void Awake()
     {
         IDragonBookUseCase dragonBookUseCase = new DragonBookUseCase();
@@ -25,6 +35,25 @@ public class GameManager : MonoBehaviour
         foreach (DragonBookScriptableObject dragonBookScriptableObject in _drakes)
         {
             dragonBookUseCase.AddCard(dragonBookScriptableObject.Map());
+        }
+    }
+
+    private void Update()
+    {
+        if (!_enabled || !Mouse.current.leftButton.isPressed)
+        {
+            return;
+        }
+        
+        Vector2 mousePosition = Mouse.current.position.ReadValue();
+        Ray ray = _camera.ScreenPointToRay(mousePosition);
+
+        int boardLayerMask = 1 << LayerMask.NameToLayer("Board");
+
+        if (Physics.Raycast(ray, out RaycastHit hit, Mathf.Infinity, boardLayerMask))
+        {
+            GameObject hitObject = hit.collider.gameObject;
+            _movableDrake.position = hitObject.transform.position;
         }
     }
 }
